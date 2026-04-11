71615f340b1d817ed8da1a07202146f244674b85
Lucas Simines <lucas.simines@gmail.com>
2026-04-11 13:12:12 -0300

    Remove migration files and migration lock for database schema updates

--- server/prisma/migrations/20260305220533_add_payment_type_to_tasks_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "PaymentType" AS ENUM ('HOURLY', 'ONE_TIME', 'INSTALLMENT', 'RECURRING');
-
--- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "paymentType" "PaymentType" NOT NULL DEFAULT 'HOURLY';
