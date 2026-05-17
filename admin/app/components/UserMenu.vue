69d5f2f4da2df3e38a8e2f1ebc0b92fcbc053864
Lucas Simines <lucas.simines@gmail.com>
2026-05-17 10:32:23 -0300

    remove theme

--- admin/app/components/UserMenu.vue
+++ admin/app/components/UserMenu.vue
@@ -21,9 +21,9 @@
     />
 
     <template #chip-leading="{ item }">
-      <div class="inline-flex items-center justify-center shrink-0 size-5">
+      <div class="inline-flex size-5 shrink-0 items-center justify-center">
         <span
-          class="rounded-full ring ring-bg bg-(--chip-light) dark:bg-(--chip-dark) size-2"
+          class="ring-bg size-2 rounded-full bg-(--chip-light) ring dark:bg-(--chip-dark)"
           :style="{
             '--chip-light': `var(--color-${(item as any).chip}-500)`,
             '--chip-dark': `var(--color-${(item as any).chip}-400)`
@@ -42,33 +42,11 @@ defineProps<{
 }>()
 
 const colorMode = useColorMode()
-const appConfig = useAppConfig()
 
 const authStore = useAuthStore()
 const { authUser } = storeToRefs(authStore)
 
 const { t } = useI18n()
-
-const colors = [
-  'red',
-  'orange',
-  'amber',
-  'yellow',
-  'lime',
-  'green',
-  'emerald',
-  'teal',
-  'cyan',
-  'sky',
-  'blue',
-  'indigo',
-  'violet',
-  'purple',
-  'fuchsia',
-  'pink',
-  'rose'
-]
-const neutrals = ['slate', 'gray', 'zinc', 'neutral', 'stone']
 
 const user = computed(() => ({
   name: authUser.value?.name,
@@ -95,55 +73,6 @@ const items = computed<DropdownMenuItem[][]>(() => [
     }
   ],
   [
-    {
-      label: t('theme'),
-      icon: 'lucide:palette',
-      children: [
-        {
-          label: 'Primary',
-          slot: 'chip',
-          chip: appConfig.ui.colors.primary,
-          content: {
-            align: 'center',
-            collisionPadding: 16
-          },
-          children: colors.map((color) => ({
-            label: color,
-            chip: color,
-            slot: 'chip',
-            checked: appConfig.ui.colors.primary === color,
-            type: 'checkbox',
-            onSelect: (e) => {
-              e.preventDefault()
-
-              appConfig.ui.colors.primary = color
-            }
-          }))
-        },
-        {
-          label: 'Neutral',
-          slot: 'chip',
-          chip:
-            appConfig.ui.colors.neutral === 'neutral' ? 'old-neutral' : appConfig.ui.colors.neutral,
-          content: {
-            align: 'end',
-            collisionPadding: 16
-          },
-          children: neutrals.map((color) => ({
-            label: color,
-            chip: color === 'neutral' ? 'old-neutral' : color,
-            slot: 'chip',
-            type: 'checkbox',
-            checked: appConfig.ui.colors.neutral === color,
-            onSelect: (e) => {
-              e.preventDefault()
-
-              appConfig.ui.colors.neutral = color
-            }
-          }))
-        }
-      ]
-    },
     {
       label: t('appearance'),
       icon: 'lucide:sun-moon',
